34ad2ded13aeab41ffb7fa3a9de1b4d2252c47a9
Bill Nixon <bnixon67@gmail.com>
2024-04-02 22:43:15 -0500

    refactor

--- cmd/totp/main.go
+++ cmd/totp/main.go
@@ -4,35 +4,98 @@
 package main
 
 import (
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/base32"
+	"errors"
+	"flag"
 	"fmt"
+	"hash"
 	"os"
 	"path"
+	"strings"
 	"time"
 
 	"github.com/bnixon67/otp"
 )
 
+const timeFormat = time.RFC3339
+
+func parseFlags() (secret string, digits uint, timeString, hash string) {
+	flag.StringVar(&secret, "secret", "", "Base32 encoded secret")
+	flag.UintVar(&digits, "digits", 6, "Number of digits")
+	flag.StringVar(&timeString, "time", time.Now().Format(timeFormat), "Timestamp for OTP (default: now)")
+	flag.StringVar(&hash, "hash", "sha1", "Hash function (sha1, sha256, sha512)")
+	flag.Parse()
+	return
+}
+
+func validateSecret(secret string) error {
+	if secret == "" {
+		return errors.New("secret flag is required")
+	}
+	return nil
+}
+
+func parseTime(timeString string) (time.Time, error) {
+	return time.Parse(timeFormat, timeString)
+}
+
+func decodeSecret(secret string) ([]byte, error) {
+	return base32.StdEncoding.DecodeString(secret)
+}
+
+func hashFunction(hashName string) (func() hash.Hash, error) {
+	switch strings.ToLower(hashName) {
+	case "sha1":
+		return sha1.New, nil
+	case "sha256":
+		return sha256.New, nil
+	case "sha512":
+		return sha512.New, nil
+	}
+	return nil, errors.New("invalid hash name")
+}
+
+func printErrorAndExit(err error, exitCode int) {
+	fmt.Fprintf(os.Stderr, "%v\n", err)
+	fmt.Printf("Usage: %s -secret base32_secret -digits number -time time -hash function\n", path.Base(os.Args[0]))
+	flag.PrintDefaults()
+	os.Exit(exitCode)
+}
+
+func printTOTP(totp string, parsedTime time.Time, digits uint, hashName string) {
+	fmt.Printf("%s %v digits=%d hash=%s\n",
+		totp, parsedTime.Format(timeFormat), digits, hashName)
+}
+
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Printf("Usage: %s BASE32_ENCODED_SECRET\n", path.Base(os.Args[0]))
-		os.Exit(1)
+	secret, digits, timeString, hashName := parseFlags()
+
+	if err := validateSecret(secret); err != nil {
+		printErrorAndExit(err, 1)
 	}
 
-	secret := os.Args[1]
-
-	// Decode the Base32 encoded secret
-	data, err := base32.StdEncoding.DecodeString(secret)
+	parsedTime, err := parseTime(timeString)
 	if err != nil {
-		fmt.Printf("Failed to decode the secret: %v\n", err)
-		os.Exit(2)
+		printErrorAndExit(err, 2)
 	}
 
-	totp, err := otp.GenerateTOTP(data, time.Now(), uint(6), nil)
+	data, err := decodeSecret(secret)
 	if err != nil {
-		fmt.Printf("Failed to generate TOTP: %v\n", err)
-		os.Exit(2)
+		printErrorAndExit(err, 3)
 	}
 
-	fmt.Println(totp)
+	hashFunc, err := hashFunction(hashName)
+	if err != nil {
+		printErrorAndExit(err, 4)
+	}
+
+	totp, err := otp.GenerateTOTP(data, parsedTime, digits, hashFunc)
+	if err != nil {
+		printErrorAndExit(err, 5)
+	}
+
+	printTOTP(totp, parsedTime, digits, hashName)
 }
